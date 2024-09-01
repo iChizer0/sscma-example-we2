ad2db9a636e992461b8882c115fcdb54dbd2b4f6
nullptr <nullptr@localhost>
2024-09-01 11:39:36 +0000

    feat: make flash, add lib sel

--- EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.mk
+++ EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.mk
@@ -1,7 +1,7 @@
 override SCENARIO_APP_SUPPORT_LIST := $(APP_TYPE)
 
 APPL_DEFINES += -DSSCMA -DHIMAX_PLATFORM
-LIB_SEL = sscma_micro tflmtag2209_u55tag2205
+LIB_SEL = sscma_micro tflmtag2209_u55tag2205 spi_eeprom pwrmgmt sensordp
 MID_SEL =
 
 override OS_SEL := freertos
