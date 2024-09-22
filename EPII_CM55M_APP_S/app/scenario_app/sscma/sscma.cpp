0a554ad11396867bb57525df1777226e7a9b9fcb
nullptr <nullptr@localhost>
2024-09-22 12:31:42 +0000

    refactor: sscma micro

--- EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.cpp
+++ EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.cpp
@@ -1,11 +1,53 @@
+#include <sscma.h>
+
+#include <cstdio>
+#include <forward_list>
+#include <string>
+#include <vector>
+
 #include "__sscma.h"
+#include "ma_codec.h"
+#include "ma_server.h"
+#include "resource.hpp"
+
+static void __task(void*) {
+    MA_LOGD(MA_TAG, "Initializing Encoder");
+    ma::EncoderJSON encoder;
+
+    MA_LOGD(MA_TAG, "Initializing ATServer");
+    ma::ATServer    server(encoder);
+
+    int ret = 0;
+
+    MA_LOGD(MA_TAG, "Initializing ATServer services");
+    ret = server.init();
+    if (ret != MA_OK) {
+        MA_LOGE(MA_TAG, "ATServer init failed: %d", ret);
+    }
+
+    MA_LOGD(MA_TAG, "Starting ATServer");
+    ret = server.start();
+    if (ret != MA_OK) {
+        MA_LOGE(MA_TAG, "ATServer start failed: %d", ret);
+    }
+
+    MA_LOGD(MA_TAG, "ATServer started");
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 extern "C" {
 
 int app_main(void) {
+    puts("Build date: " __DATE__ " " __TIME__);
+    if (xTaskCreate(__task, "__task", 20480, NULL, 3, NULL) != pdPASS) {
+        puts("__task creation failed!");
+        while (1) {
+        }
+    }
 
-
-
+    vTaskStartScheduler();
 
     return 0;
 }
--- EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.mk
+++ EPII_CM55M_APP_S/app/scenario_app/sscma/sscma.mk
@@ -1,7 +1,7 @@
 override SCENARIO_APP_SUPPORT_LIST := $(APP_TYPE)
 
-APPL_DEFINES += -DSSCMA -DHIMAX_PLATFORM
-LIB_SEL = sscma_micro tflmtag2209_u55tag2205 spi_eeprom pwrmgmt sensordp sensordp
+APPL_DEFINES += -DSSCMA -DHIMAX_PLATFORM -DIP_xdma
+LIB_SEL = sscma_micro  tflmtag2209_u55tag2205 spi_eeprom pwrmgmt sensordp 
 MID_SEL =
 
 override OS_SEL := freertos_10_5_1
@@ -11,6 +11,7 @@ override TRUSTZONE := y
 override TRUSTZONE_TYPE := security
 override TRUSTZONE_FW_TYPE := 1
 override EPII_USECASE_SEL := drv_user_defined
+override CIS_SEL := HM_COMMON
 override HIMAX_PLATFORM := y
 
 CIS_SUPPORT_INAPP = cis_sensor
@@ -18,12 +19,6 @@ CIS_SUPPORT_INAPP_MODEL = cis_ov5647
 
 ifeq ($(strip $(TOOLCHAIN)), arm)
 override LINKER_SCRIPT_FILE := $(SCENARIO_APP_ROOT)/$(APP_TYPE)/sscma.sct
-else#TOOLChain
+else
 override LINKER_SCRIPT_FILE := $(SCENARIO_APP_ROOT)/$(APP_TYPE)/sscma.ld
 endif
-
-##
-# Add new external device here
-# The source code should be located in ~\external\{device_name}\
-##
-#EXT_DEV_LIST += 
